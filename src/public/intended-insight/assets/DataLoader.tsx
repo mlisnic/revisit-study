0de08782534edf366d81926553b44bb4bb605643
mlisnic <mlisnic@ya.ru>
2025-03-06 14:47:07 -0700

    language tweaks

--- src/public/intended-insight/assets/DataLoader.tsx
+++ src/public/intended-insight/assets/DataLoader.tsx
@@ -28,15 +28,15 @@ export default {
     source:
       'Global Influenza Surveillance and Response System (GISRS), report published by the World Health Organization (WHO).',
     warning:
-      'Do not make causal conclusions about the patterns in data. There are many factors at play.',
+      'Avoid drawing causal conclusions from these data patterns, as multiple factors may influence the trends observed.',
     counter:
       'The observed drop in cases is likely a result of preventive measures in place due to COVID lockdowns.',
     guide:
-      'The WHO surveillance data is collected in order to track the dominant strains of flu. This allows scientists to identify the most effective vaccine type for the season and allocate resources according to need.',
+      'The WHO surveillance data is collected in order to track the dominant strains of flu. This allows scientists to identify the most effective vaccine type for the season and allocate resources accordingly.',
     intended:
       'Based on the chart above, the vaccine for this season should include both A and B strains of flu as both are common.',
     unintended:
-      'Based on the chart above, flu infections got potentially misclassified as COVID.',
+      'Based on the chart above, flu infections may have been erroneously misclassified as COVID.',
   },
   covidnet: {
     image: covidnet,
@@ -48,7 +48,7 @@ export default {
     counter:
       'The rise in vaccinated hospitalizations primarily reflects increases in vaccine coverage and higher coverage in older adults.',
     guide:
-      'This chart is from a CDC report on improving how we communicate vaccine effectiveness. As more people get vaccinated, it is expected that cases and hospitalizations among the vaccinated will rise despite no change in vaccine effectiveness. This highlights the need to update public communication on breakthrough cases and emphasize that they are not a reason for panic.',
+      'This chart is from a CDC report on improving communication about vaccine effectiveness. As more people get vaccinated, it is expected that cases and hospitalizations among the vaccinated will rise despite no change in vaccine effectiveness. This highlights the need to update public communication on breakthrough cases and emphasize that they are not a reason for panic.',
     intended:
       'Based on the chart above, breakthrough cases become more common as more people get vaccinated.',
     unintended:
@@ -59,11 +59,11 @@ export default {
     title: '',
     source: 'Article published by The Washington Post.',
     warning:
-      'Zooming out on the entire time series does not reflect the recent trends in climate.',
+      'Zooming out on the entire time series does not reflect recent trends in climate.',
     counter:
       'Although the climate has varied over the course of history, we are currently experiencing the fastest recorded increase in temperature.',
     guide:
-      'This chart presents the most detailed reconstruction of Earth’s temperatures over the past 485 million years, revealing a history of dramatic shifts in climate. This research provides a deeper understanding of how past environments influenced life on Earth and opens new questions about the mechanisms driving long-term climate stability and change.',
+      'This chart presents the most detailed reconstruction of Earth’s temperatures over the past 485 million years, revealing a history of dramatic shifts in climate before humans existed. This research provides a deeper understanding of how past environments influenced life on Earth and opens new questions about the mechanisms driving climate stability and change in the current geological stage.',
     intended:
       'Based on the chart above, climate has varied a lot over the history of the Earth.',
     unintended:
@@ -77,9 +77,9 @@ export default {
     warning:
       'We cannot make determinations about the relative risk of contracting COVID-19 based on this map, since the data is not normalized by population count.',
     counter:
-      'The states with highest raw COVID-19 case numbers are predominantly states with higher population counts.',
+      'The states with most reported cases are those with higher population counts, while relative risk depends on cases per capita.',
     guide:
-      'This map helps CDC officials and responders quickly identify where case numbers are highest, guiding decisions on deploying medical personnel, scaling up testing, and allocating resources. It’s a tool for prioritizing support where the demand is greatest, ensuring that overwhelmed areas get the assistance they need.',
+      'This map helps CDC officials and responders quickly identify where case numbers are highest, guiding decisions on deploying medical personnel, medication, or testing kits. It’s a tool for prioritizing support where the demand is greatest, ensuring that overwhelmed areas get the assistance they need.',
     intended:
       'Based on the chart above, more medical resources should be allocated to states colored in purple than those in yellow.',
     unintended:
